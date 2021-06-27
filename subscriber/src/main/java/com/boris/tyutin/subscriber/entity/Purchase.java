6915d4cbe76b26c1d83147dd64771f27f82b7656
Tyutin Boris <boris919323@gmail.com>
2021-06-27 16:28:10 +0300

    add entity

--- subscriber/src/main/java/com/boris/tyutin/subscriber/entity/Purchase.java
+++ subscriber/src/main/java/com/boris/tyutin/subscriber/entity/Purchase.java
@@ -1,8 +1,36 @@
-//package com.boris.tyutin.subscriber.entity;
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//
-//public class Purchase {
-//}
+package com.boris.tyutin.subscriber.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "purchase")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long msisdn;
+
+    @Column(nullable = false)
+    private String action;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
--- subscriber/src/main/java/com/boris/tyutin/subscriber/entity/Subscription.java
+++ subscriber/src/main/java/com/boris/tyutin/subscriber/entity/Subscription.java
@@ -1,4 +1,36 @@
-//package com.boris.tyutin.subscriber.entity;
-//
-//public class Subscription {
-//}
+package com.boris.tyutin.subscriber.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "subscription")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long msisdn;
+
+    @Column(nullable = false)
+    private String action;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
